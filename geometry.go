0831d5114f1da24c86f50f81a5fb77bfe8f34222
Paul Quique <48380381+paul-quique@users.noreply.github.com>
2022-07-05 10:03:30 +0200

    added documentation

--- geometry.go
+++ geometry.go
@@ -6,11 +6,13 @@ import (
 	"github.com/google/uuid"
 )
 
+//Dist returns the distance between two points
 func Dist(p1, p2 *Point) float64 {
 	x1, x2, y1, y2 := p1.X, p2.X, p1.Y, p2.Y
 	return math.Sqrt(math.Pow(x2-x1, 2) + math.Pow(y2-y1, 2))
 }
 
+//RandomID returns a new random unique ID
 func RandomID() string {
 	return uuid.New().String()
 }
