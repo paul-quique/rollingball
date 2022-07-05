2adda360effeb3ce451c67fe9ca42af4e7a9f438
Paul Quique <48380381+paul-quique@users.noreply.github.com>
2022-07-05 15:03:00 +0200

    Update viewport.go

--- viewport.go
+++ viewport.go
@@ -26,10 +26,10 @@ func (v *Viewport) MultiplyZoom(zX, zY float64) {
 	dx := dist(v.MinX, v.MaxX) * 0.5 * (v.ZoomFactorX - 1)
 	dy := dist(v.MinY, v.MaxY) * 0.5 * (v.ZoomFactorX - 1)
 
-	v.MinX -= dx
-	v.MaxX += dx
-	v.MinY -= dy
-	v.MaxY += dy
+	v.MinX += dx
+	v.MaxX -= dx
+	v.MinY += dy
+	v.MaxY -= dy
 
 }
 
