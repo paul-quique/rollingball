49d11d335958a9d8ab801ee74e4263cc326c3684
Paul Quique <48380381+paul-quique@users.noreply.github.com>
2022-07-05 13:28:49 +0200

    Update viewport.go

--- viewport.go
+++ viewport.go
@@ -3,32 +3,48 @@ package rollingball
 //Viewport stores the area to render
 type Viewport struct {
 	MinX, MinY, MaxX, MaxY float64
-	Width, Height          float64 //width and height in pixels
+	Width, Height          float64
+	ZoomFactorX            float64
+	ZoomFactorY            float64
 }
 
-//SetMinX sets the value of MinX
-func (v *Viewport) SetMinX(x float64) {
-	v.MinX = x
+//GetZoom returns the zoom values for the viewport
+func (v *Viewport) GetZoom() (zX float64, zY float64) {
+	return v.ZoomFactorX, v.ZoomFactorY
 }
 
-//SetMinY sets the value of MinX
-func (v *Viewport) SetMinY(y float64) {
-	v.MinY = y
+//SetZoom sets the zoom values for the viewport
+func (v *Viewport) SetZoom(zX, zY float64) {
+	v.ZoomFactorX, v.ZoomFactorY = zX, zY
 }
 
-//SetMaxX sets the value of MaxX
-func (v *Viewport) SetMaxX(x float64) {
-	v.MaxX = x
+//MultiplyZoom multiply both x and y zoom factors by zX and zY
+func (v *Viewport) MultiplyZoom(zX, zY float64) {
+	v.ZoomFactorX *= zX
+	v.ZoomFactorY *= zY
+	v.MinX *= zX
+	v.MinY *= zY
+	v.MaxX *= zX
+	v.MaxY *= zY
 }
 
-//SetMaxY sets the value of MaxY
-func (v *Viewport) SetMaxY(y float64) {
-	v.MaxX = y
+func (v *Viewport) MoveX(dx float64) {
+	//zoom factor does not change
+	v.MinX += dx * v.ZoomFactorX
+	v.MaxX += dx * v.ZoomFactorX
 }
 
-//GetScale calculates and returns the scale values for the viewport
-func (v Viewport) GetScale() (scaleX float64, scaleY float64) {
-	scaleX = v.Width / (v.MaxX - v.MinX)
-	scaleY = v.Height / (v.MaxY - v.MinY)
-	return
+func (v *Viewport) MoveY(dy float64) {
+	//zoom factor does not change
+	v.MinX += dy * v.ZoomFactorY
+	v.MaxX += dy * v.ZoomFactorY
+}
+
+func (v *Viewport) SetBounds(x1, y1, x2, y2 float64) {
+	v.MinX = x1
+	v.MinY = y1
+	v.MaxX = x2
+	v.MaxY = y2
+	v.ZoomFactorX = (x2 - x1) / v.Width
+	v.ZoomFactorY = (y2 - y1) / v.Height
 }
