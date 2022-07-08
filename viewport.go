a6dd23b09848bc5c17403a85fbe12a24b906567a
Paul Quique <48380381+paul-quique@users.noreply.github.com>
2022-07-08 15:56:55 +0200

    viewport is working as expected

--- viewport.go
+++ viewport.go
@@ -7,16 +7,20 @@ import (
 
 //Viewport stores the area to render
 type Viewport struct {
-	OffsetX, OffsetY       float64
-	MinX, MinY, MaxX, MaxY float64
-	Width, Height          float64
-	ZoomFactorX            float64
-	ZoomFactorY            float64
+	OffsetX, OffsetY         float64
+	MinX, MinY, MaxX, MaxY   float64
+	Width, Height            float64
+	ZoomFactorX, ZoomFactorY float64
 }
 
 //SetZoom sets the zoom values for the viewport
 func (v *Viewport) SetZoom(zX, zY float64) {
 	v.ZoomFactorX, v.ZoomFactorY = zX, zY
+
+	v.MinX = v.Width/2 - v.Width*(1./(2*v.ZoomFactorX))
+	v.MaxX = v.Width/2 + v.Width*(1./(2*v.ZoomFactorX))
+	v.MinY = v.Height/2 - v.Width*(1./(2*v.ZoomFactorY))
+	v.MaxY = v.Height/2 + v.Width*(1./(2*v.ZoomFactorY))
 }
 
 //MultiplyZoom multiply both x and y zoom factors by zX and zY
@@ -30,23 +34,17 @@ func (v *Viewport) MultiplyZoom(zX, zY float64) {
 	v.MaxY = v.Height/2 + v.Width*(1./(2*v.ZoomFactorY))
 }
 
+//MoveX moves the viewport along the x-axis
 func (v *Viewport) MoveX(dx float64) {
 	v.OffsetX += dx
 }
 
+//MoveY moves the viewport along the y-axis
 func (v *Viewport) MoveY(dy float64) {
 	v.OffsetY += dy
 }
 
-func (v *Viewport) SetBounds(x1, y1, x2, y2 float64) {
-	v.MinX = x1
-	v.MinY = y1
-	v.MaxX = x2
-	v.MaxY = y2
-	v.ZoomFactorX = (x2 - x1) / v.Width
-	v.ZoomFactorY = (y2 - y1) / v.Height
-}
-
+//Update updates the viewport if certain keys are pressed
 func (v *Viewport) Update() {
 	if ebiten.IsKeyPressed(ebiten.KeyArrowRight) {
 		v.MoveX(-1)
@@ -67,11 +65,18 @@ func (v *Viewport) Update() {
 	if inpututil.IsKeyJustReleased(ebiten.KeyBackspace) {
 		v.MultiplyZoom(0.8, 0.8)
 	}
+
+	if inpututil.IsKeyJustReleased(ebiten.KeySpace) && inpututil.IsKeyJustReleased(ebiten.KeyBackspace) {
+		v.SetZoom(1, 1)
+	}
 }
+
+//GetMinPosX returns the minimum X value of the viewport
 func (v *Viewport) GetMinPosX() float64 {
 	return v.MinX + v.OffsetX
 }
 
+//GetMinPosY returns the minimum Y value of the viewport
 func (v *Viewport) GetMinPosY() float64 {
 	return v.MinY + v.OffsetY
 }
