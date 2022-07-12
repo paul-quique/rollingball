ed2077afc862bfb67e5f887165fff34840a8fc47
Paul Quique <48380381+paul-quique@users.noreply.github.com>
2022-07-12 14:39:20 +0200

    Update viewport.go

--- viewport.go
+++ viewport.go
@@ -84,7 +84,8 @@ func (v *Viewport) GetMinPosY() float64 {
 //GetMousePosition computes and returns the mouse position in the game
 func (v *Viewport) GetMousePosition() (x, y float64) {
 	cx, cy := ebiten.CursorPosition()
-	return float64(cx)/v.ZoomFactorX + v.GetMinPosX(), float64(cy)/v.ZoomFactorY + v.GetMinPosY()
+	f := ebiten.DeviceScaleFactor()
+	return (float64(cx)/v.ZoomFactorX + v.GetMinPosX()) / f, (float64(cy)/v.ZoomFactorY + v.GetMinPosY()) / f
 }
 
 func dist(a, b float64) float64 {
